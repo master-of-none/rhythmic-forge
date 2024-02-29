6c18d6a390fada4d82bacf53855c14c0af0f6881
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-02-28 23:09:55 -0800

    Modified kick sound

--- drum_beat.py
+++ drum_beat.py
@@ -2,15 +2,17 @@ import numpy as np
 import sounddevice as sd
 from scipy.signal import butter, sosfilt
 
+
 def kick(frequency, duration):
     sample_rate = 44100
-    time = np.linspace(0, duration, sample_rate)
-    exponential_decay = np.exp(-20 * time)
-    oscillation = np.cos(2 * np.pi * frequency * time)
-    envelope = np.sqrt(time) * oscillation * exponential_decay
+    time = np.linspace(0, 1, int(sample_rate * duration / 1000))
+    exponential_decay = np.power(0.5, 25 * time)
+    envelope = frequency * np.sqrt(time) - 0.15
+    oscillation = exponential_decay * np.cos(envelope)
     sos = butter(2, 300, 'low', fs=sample_rate, output='sos')
-    filtered = sosfilt(sos, envelope)
+    filtered = sosfilt(sos, oscillation)
     return filtered * 3
+
 
 if __name__ == '__main__':
     frequency = 30
