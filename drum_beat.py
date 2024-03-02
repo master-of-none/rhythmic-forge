146b46cd89bbdb070ed65927f486988d6e7890f8
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-03-01 18:54:40 -0800

    Added hi hat sound

--- drum_beat.py
+++ drum_beat.py
@@ -67,27 +67,45 @@ class HiHat:
     def __init__(self, sample_rate=44100):
         self.sample_rate = sample_rate
 
-    def generate_noise(self):
-        pass
+    def generate_noise(self, duration):
+        noise = np.random.random_sample(int((self.sample_rate / 1000) * duration))
+        return noise
 
-    def generate_square_tone(self):
-        pass
+    def generate_square_tone(self, frequency, duration):
+        time = np.linspace(0, 1, int((self.sample_rate / 1000) * duration))
+        envelope = np.power(0.5, 25 * time)
+        oscillation = np.sin(2 * np.pi * frequency * time)
+        square_tone = np.where(oscillation > 0, 1, -1) * envelope
+        return square_tone
 
     def create_filter(self):
-        pass
+        sos_high_pass_1 = butter(10, 100, 'hp', fs=1000, analog=False, output='sos')
+        sos_high_pass_2 = butter(2, 100, 'hp', fs=1000, analog=False, output='sos')
+        return sos_high_pass_1, sos_high_pass_2
 
-    def generate_hi_hat(self):
-        pass
+    def generate_hi_hat(self, duration):
+        time = np.linspace(1, 0, int((self.sample_rate / 1000) * duration))
+        time = np.power(time, 4)
+        high_noise = self.generate_square_tone(350, duration) + self.generate_square_tone(800, duration) + \
+                     (self.generate_noise(duration) / 4)
+        sos_high_pass_1, sos_high_pass_2 = self.create_filter()
+        filtered_wave_1 = sosfilt(sos_high_pass_1, high_noise)
+        filtered_wave_2 = sosfilt(sos_high_pass_2, filtered_wave_1)
+        hi_hat_sound = filtered_wave_2 * time * 4
+        wavfile.write('hi_hat_sound.wav', self.sample_rate, hi_hat_sound)
 
 
 if __name__ == '__main__':
-    pass
-#     kick = Kick()
-#     frequency = 30
-#     duration = 150
-#     sound = kick.generate_kick_sound(frequency, duration)
-#     # sd.play(sound, kick.sample_rate)
-#     # sd.wait()
-#     frequency = 250
-#     snare = Snare()
-#     snare.generate_snare_sound(frequency, duration)
+    hi_hat = HiHat()
+    duration = 150
+    hi_hat.generate_hi_hat(duration)
+
+    #     kick = Kick()
+    #     frequency = 30
+    #     duration = 150
+    #     sound = kick.generate_kick_sound(frequency, duration)
+    #     # sd.play(sound, kick.sample_rate)
+    #     # sd.wait()
+    #     frequency = 250
+    #     snare = Snare()
+    #     snare.generate_snare_sound(frequency, duration)
