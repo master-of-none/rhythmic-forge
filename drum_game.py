2a909eecae5334373a625172017c6c227d392ecc
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-03-12 15:13:43 -0700

    Added box for each sound

--- drum_game.py
+++ drum_game.py
@@ -10,6 +10,9 @@ GREEN = (0, 255, 0)
 GOLD = (212, 175, 55)
 BLUE = (0, 255, 255)
 FPS = 60
+INSTRUMENTS = 6
+BEATS = 8
+
 
 # Pyagame intialize
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
@@ -19,10 +22,36 @@ label_font = pygame.font.Font('freesansbold.ttf', size=28)
 
 running = True
 
+def draw_grid():
+    # Draw grid lines and labels
+    for i in range(INSTRUMENTS):
+        pygame.draw.line(screen, GREY, (0, (i * 100) + 100), (200, (i * 100) + 100), 2)
+
+        # Draw labels
+    labels = ["Kick", "Snare", "Hi Hat", "Open Hat", "Wood Block", "Mid Tom"]
+    for i, label in enumerate(labels):
+        text = label_font.render(label, True, WHITE)
+        screen.blit(text, (30, i * 100 + 30))
+
+    all_box = []
+    for i in range(BEATS):
+        for j in range(INSTRUMENTS):
+            color = GREY
+            rect = pygame.draw.rect(screen, color,
+                                    [i * ((WIDTH - 200) // BEATS) + 205, (j * 100) + 5, ((WIDTH - 200) // BEATS) - 10,
+                                     ((HEIGHT - 200) // INSTRUMENTS) - 10], 0, 3)
+            pygame.draw.rect(screen, BLACK,
+                             [i * ((WIDTH - 200) // BEATS) + 200, (j * 100), ((WIDTH - 200) // BEATS),
+                             ((HEIGHT - 200) // INSTRUMENTS)], 2, 5)
+
+            all_box.append((rect, (i, j)))
+
 while running:
     clock.tick(FPS)
     screen.fill(BLACK)
 
+
+    draw_grid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
