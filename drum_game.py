b01f82554a4d74ada824c88b38b72d0482a6a6af
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-03-12 17:04:29 -0700

    Added playing notes. Added play pause and clear button. Changed positions.

--- drum_game.py
+++ drum_game.py
@@ -9,11 +9,13 @@ GREY = (128, 128, 128)
 GREEN = (0, 255, 0)
 GOLD = (212, 175, 55)
 BLUE = (0, 255, 255)
+RED = (255, 0, 0)
 FPS = 60
 INSTRUMENTS = 6
 BEATS = 8
+BPM = 240
 
-# Pyagame intialize
+# Pygame initialize
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Custom Beat Maker")
 clock = pygame.time.Clock()
@@ -22,13 +24,33 @@ label_font = pygame.font.Font('freesansbold.ttf', size=28)
 running = True
 clicked = [[-1 for _ in range(BEATS)] for _ in range(INSTRUMENTS)]
 
+mixer.init()
+sounds = {
+    0: mixer.Sound('generatedSounds/kick.wav'),
+    1: mixer.Sound('generatedSounds/snare_sound.wav'),
+    2: mixer.Sound('generatedSounds/hi_hat_sound.wav'),
+    3: mixer.Sound('generatedSounds/open_hat_sound.wav'),
+    4: mixer.Sound('generatedSounds/woodblock.wav'),
+    5: mixer.Sound('generatedSounds/mid_tom_sound.wav')
+}
 
-def draw_grid(clicks):
-    # Draw grid lines and labels
+active_beat = 0
+beat_changed = True
+playing = True
+active_length = 0
+
+
+def play_notes():
     for i in range(INSTRUMENTS):
-        pygame.draw.line(screen, GREY, (0, (i * 100) + 100), (200, (i * 100) + 100), 2)
+        if clicked[i][active_beat] == 1:
+            sounds[i].play()
 
-        # Draw labels
+
+def draw_grid(clicks, beat):
+
+    for i in range(INSTRUMENTS):
+        pygame.draw.line(screen, RED, (0, (i * 100) + 100), (200, (i * 100) + 100), 2)
+
     labels = ["Kick", "Snare", "Hi Hat", "Open Hat", "Wood Block", "Mid Tom"]
     for i, label in enumerate(labels):
         text = label_font.render(label, True, WHITE)
@@ -46,24 +68,71 @@ def draw_grid(clicks):
                               ((HEIGHT - 200) // INSTRUMENTS)], 2, 5)
 
             all_box.append((rect, (i, j)))
+
+    pygame.draw.rect(screen, BLUE, [beat * ((WIDTH - 200) // BEATS) + 200, 0, ((WIDTH - 200) // BEATS),
+                                    INSTRUMENTS * 100], 5, 3)
+
     return all_box
+
 
 while running:
     clock.tick(FPS)
     screen.fill(BLACK)
 
-    all_box = draw_grid(clicked)
+    total_box = draw_grid(clicked, active_beat)
+
+    # Play and Pause
+    play_pause = pygame.draw.rect(screen, GREY, [30, HEIGHT - 80, 250, 80], 0, 5)
+    play_text = label_font.render('Play and Pause', True, WHITE)
+    screen.blit(play_text, (50, HEIGHT - 80))
+
+    if playing:
+        play_text_2 = label_font.render("Playing", True, WHITE)
+    else:
+        play_text_2 = label_font.render("Pause", True, WHITE)
+
+    screen.blit(play_text_2, (70, HEIGHT - 50))
+
+    clear_button = pygame.draw.rect(screen, GREY, [1150, HEIGHT - 80, 250, 80], 0, 5)
+    clear_text = label_font.render("Clear Board", True, WHITE)
+    screen.blit(clear_text, (1160, HEIGHT - 50))
+    if beat_changed:
+        play_notes()
+        beat_changed = False
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Toggle button state when clicked
-            for box, coordinates in all_box:
+            for box, coordinates in total_box:
                 if box.collidepoint(event.pos):
                     x, y = coordinates
                     clicked[y][x] *= -1
 
+        if event.type == pygame.MOUSEBUTTONUP:
+            if play_pause.collidepoint(event.pos):
+                if playing:
+                    playing = False
+                elif not playing:
+                    playing = True
+
+            elif clear_button.collidepoint(event.pos):
+                clicked = [[-1 for _ in range(BEATS)] for _ in range(INSTRUMENTS)]
+
+    beat_len = 3600 // BPM
+    if playing:
+        if active_length < beat_len:
+            active_length += 1
+        else:
+            active_length = 0
+            if active_beat < BEATS - 1:
+                active_beat += 1
+                beat_changed = True
+            else:
+                active_beat = 0
+                beat_changed = True
     pygame.display.flip()
 
 pygame.quit()
